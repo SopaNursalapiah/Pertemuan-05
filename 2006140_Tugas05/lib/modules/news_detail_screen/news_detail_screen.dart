37a9ca245a7aab86aaedf2652fb4bdf0562e707d
SopaNursalapiah <129405280+SopaNursalapiah@users.noreply.github.com>
2023-03-31 14:26:07 +0700

    Add files via upload

---
+++ 2006140_Tugas05/lib/modules/news_detail_screen/news_detail_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class NewsDetailScreen extends StatefulWidget {
+  const NewsDetailScreen({
+    super.key,
+    required this.id,
+  });
+  final String id;
+
+  @override
+  State<NewsDetailScreen> createState() => _NewsDetailScreenState();
+}
+
+class _NewsDetailScreenState extends State<NewsDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://i.pinimg.com/originals/be/cd/2c/becd2c4dbf9efb05e0d8471eed688a0d.png",
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                   const SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment:MainAxisAlignment.center ,
+                      children: const [
+                        Text(
+                          'Panda Cute',
+                           style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  const Text(
+                    'Nyam Nyam Nyam',
+                    
+                    maxLines: 10,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                  ],
+                ),
+              ),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
+
+class NewsDetailScreenHot extends StatefulWidget {
+  const NewsDetailScreenHot({
+    super.key, 
+    required this.newsDetailHot,
+  });
+  final String newsDetailHot;
+
+  @override
+  State<NewsDetailScreenHot> createState() => _NewsDetailScreenHotState();
+}
+
+class _NewsDetailScreenHotState extends State<NewsDetailScreenHot> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://cdn.dribbble.com/users/4127921/screenshots/15566620/cute_panda_cartoon_4x.jpg",
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                   const SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment:MainAxisAlignment.center ,
+                      children: const [
+                        Text(
+                          'Hai hai',
+                           style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  const Text(
+                    '',
+                    maxLines: 10,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                  ],
+                ),
+              ),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
